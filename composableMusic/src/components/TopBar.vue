19cc95a1c8131252c22fba2c44f77862eb86b93a
fbsc1999 <filipebsc1999@gmail.com>
2022-12-08 12:04:08 +0000

    Connect Wallet no Top Bar

--- composableMusic/src/components/TopBar.vue
+++ composableMusic/src/components/TopBar.vue
@@ -12,18 +12,82 @@
 
         <v-spacer></v-spacer>
         <v-column>
+            <!--Icone search-->
             <v-btn icon>
                 <v-icon style="color: #FDFDFD">mdi-magnify</v-icon>
             </v-btn>
+            
+            <!--Icone e menu wallet-->
+            <v-menu
+                :close-on-content-click="false"
+            >
+                <template v-slot:activator="{ props }">
+                <v-btn v-bind="props">
+                    <v-icon style="color: #FDFDFD">mdi-credit-card-outline</v-icon>
+                </v-btn>
+                </template>
                 
-            <v-btn icon>
-                <v-icon style="color: #FDFDFD">mdi-credit-card-outline</v-icon>
-            </v-btn>
+                <v-list style=" backgroundColor: #242121">
+                    <!--Titulo menu:-->
+                    <v-list-item>
+                        <p v-if="walletAddress==''" class="font-weight-medium text-white bg-dark">Connect Wallet:</p>
+                        <p v-else id="walletButton" class="font-weight-medium text-white bg-dark">Wallet Connected:</p>
+                    </v-list-item>
 
+                    <!--Connect Wallet:-->
+                    <v-list-item>
+                        <v-container fill-height fluid>
+                            <v-card>
+                                <!----<v-card-text class="text-prop mt-10" style="font-size: 3vh">
+                                    <v-row align="center" justify="center">
+                                        <p>Connect Metamask</p>
+                                    </v-row>
+                                </v-card-text>
+                                <v-row class="pt-10 pb-10" align="center" justify="center">
+                                    <v-btn v-if="!connected" @click="connect()">Connect Metamask</v-btn>
+                                    <v-btn v-else @click="mint()">Mint NFT</v-btn>
+                                </v-row>-->
+
+                                <div className="Minter">
+                                    <!--botão para conectar carteira:-->
+                                    <v-btn 
+                                        color=#00E676
+                                        :width=200
+                                        v-if="walletAddress==''" 
+                                        id="walletButton" 
+                                        @click="connectWalletPressed()">
+                                        
+                                        <span>Connect Wallet</span>
+                                    </v-btn>
+                                    <!--botão carteira já conectada:-->
+                                    <v-btn 
+                                        color=#00E676
+                                        v-else id="walletButton" 
+                                        @click="connectWalletPressed()">
+                                        
+                                        <span>Connected: {{this.walletAddress}}</span>
+                                    </v-btn>
+
+                                    <p>
+                                        {{this.status}}
+                                    </p>
+                                </div>
+                            </v-card>
+                            
+                            
+                        </v-container>
+                    </v-list-item>
+
+
+                </v-list>
+            </v-menu>
+
+            <!--Icone notificacoes:-->
             <v-btn icon>
                 <v-icon style="color: #FDFDFD">mdi-bell</v-icon>
             </v-btn>
 
+            <!--Icone e menu login:-->
             <v-menu
                 :close-on-content-click="false"
                 :width=500 
@@ -83,6 +147,7 @@
                 </v-list>
             </v-menu>
 
+            <!--Icone Carrinho:-->
             <v-btn icon>
                 <v-icon style="color: #FDFDFD">mdi-cart-minus</v-icon>
             </v-btn>
@@ -92,35 +157,45 @@
 </template>
 
 <script>
+  import {connectWallet, getCurrentWalletConnected, mintNFT} from "../utils/metamask.js"
+
   export default {
-    data: () => ({
-    }),
-  }
+    data(){
+        return{
+            description: "",
+            name: "",
+            status: "",
+            url: "",
+            walletAddress: "",
+            /*value: "0.02ETH",
+            nameSample: "Sample Music: Jazz Music",
+            metadata: {
+                loudness: "",
+            },*/
+        }
+    },
+
+    //Utilizador Novo conecta -> this.walletAddress "" -> HTTP POST do Endereço
+    //Utilizador Antigo conecta -> this.walletAddress " -> HTTP GET dos dados para aquele endereço
+    // POST para dados de carteira
+    
+    async created() {
+        const {address, status} = await getCurrentWalletConnected()
+        this.walletAddress = address
+        //this.status = status
+    },
+
+    methods:{
+        async connectWalletPressed(){
+            const walletResponse = await connectWallet();
+            //this.status = walletResponse.status
+            this.walletAddress = walletResponse.address
+        },
+    }
+}
 </script>
 
-<!--
-<script>
-    export default {
-      data () {
-        return {
-          loading: [],
-        }
-      },
-      methods: {
-        load (i) {
-          this.loading[i] = true
-          setTimeout(() => (this.loading[i] = false), 3000)
-        },
-      },
-    }
-  </script>
--->
-
 <style scoped>
-/*.top_bar{
-    background-color: #212121;
-}*/
-
 .top_bar_title{
 font-family: 'Poppins';
 font-style: normal;
